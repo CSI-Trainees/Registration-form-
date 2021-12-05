77cefb1167498d475c61eac9a1e0a61449ddb6f6
John Doe <prerit1913059@akgec.ac.in>
2021-12-05 21:19:01 +0530

    Ma ka loda

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Form from "./Form";
+import { Routes ,Route } from 'react-router-dom';
+import Submit from "../src/Submit";
+
+const App = () => {
+    return(
+        <>
+            <div id="main">
+                <div id="main1">
+                    <div id="mainleft">
+                        
+                        
+                        
+                    </div>
+                    <div id="mainright">
+                        
+                        <div id="formm">
+                    <Routes>
+                <Route exact path='/' element={<Form/>} />
+                <Route path='/Submit' element={<Submit/>} />
+                </Routes>
+                        </div>
+                    </div>
+
+                </div>
+                
+            </div>
+             {/* <Routes>
+                <Route exact path='/' element={<Form/>} />
+                <Route path='/submit' element={<Submit/>} />
+                </Routes> */}
+           
+        </>
+    );
+}
+
+export default App;
---
+++ src/Submit.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Submit.css"
+export default function Submit() {
+    return (
+        <div>
+            <div className="wrap">
+                <img src="./img/Group 56(1).svg" alt="logo" />
+         <h1>Thanks For Registering!!</h1>
+         <hr/>
+    </div>
+        </div>
+    )
+}
